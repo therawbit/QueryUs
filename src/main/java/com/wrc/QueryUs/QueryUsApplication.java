2fdac71839db93ce3558a0620f74c77cc70f3fe9
Sudarshan <sudarshandevkota22@gmail.com>
2022-09-28 12:43:28 +0545

    project started

---
+++ src/main/java/com/wrc/QueryUs/QueryUsApplication.java
@@ -0,0 +1,13 @@
+package com.wrc.QueryUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wrc/QueryUs/QueryUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrc.QueryUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
