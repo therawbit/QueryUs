c10bfc07af359551a4c7a061abf6add85dbf13c0
Sudarshan <sudarshandevkota22@gmail.com>
2022-10-16 12:02:14 +0545

    answer entity created

---
+++ src/main/java/com/wrc/QueryUs/entity/Answer.java
@@ -0,0 +1,26 @@
+package com.wrc.QueryUs.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Answer {
+    @Id @GeneratedValue
+    private int id;
+
+    private String answer;
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
